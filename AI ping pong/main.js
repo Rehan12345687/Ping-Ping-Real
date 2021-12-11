c61d746b99494fa56f1ee88231947a10a9b84c11
Rehan12345687 <89651604+Rehan12345687@users.noreply.github.com>
2021-12-11 12:27:54 +0530

    Add files via upload

---
+++ AI ping pong/main.js
@@ -0,0 +1,14 @@
+rightwrist_x = "";
+rightwrist_y = "";
+score_wrightwirst = "";
+function preload(){}
+function setup(){
+    canvas = createCanvas(700,600)
+    canvas.parent('canvas_holder');
+}
+function draw(){}
+function gotPoses(){
+            r = random(255);
+            g = random(255);   
+            b = random(255);
+}
